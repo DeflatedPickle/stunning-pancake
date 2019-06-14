21b4a4e735136e63467f7725c1b7ad275d443a3e
DeflatedPickle <DeflatedPickle@gmail.com>
2019-06-14 12:50:37 +0100

    Added jumping and polished the jar model more

---
+++ SlimeGame/Assets/Generated/InputMaster.cs
@@ -0,0 +1,276 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class InputMaster : IInputActionCollection
+{
+    private InputActionAsset asset;
+    public InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""2c2b8511-137b-4634-a7d2-8dd71c34634b"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""id"": ""3c8ea6d5-44fb-46a4-8eb5-f64e9dc33698"",
+                    ""expectedControlLayout"": """",
+                    ""continuous"": false,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""bindings"": []
+                },
+                {
+                    ""name"": ""Movement"",
+                    ""id"": ""a4f04e1e-c80c-4930-9b7e-72f6576be2c6"",
+                    ""expectedControlLayout"": """",
+                    ""continuous"": false,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""bindings"": []
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8490a74c-1abe-4ad4-969d-748d1fb540d0"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": "";Keyboard and Mouse"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""f672d927-2d35-4dae-8dfe-3b84f1eac110"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""16a147ab-d323-4682-9fa8-ff16f0003aed"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""GamePad;Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""9f1bff9f-a774-438a-b2d1-f6f666a48fdb"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""GamePad;Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""a2c8b9e0-4e10-43e2-9316-9ee54126ddbd"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""GamePad;Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""e6e80871-f3a7-442e-94a5-203d90446764"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""GamePad;Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Gamepad"",
+            ""basedOn"": """",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Keyboard and Mouse"",
+            ""basedOn"": """",
+            ""bindingGroup"": ""Keyboard and Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player
+        m_Player = asset.GetActionMap("Player");
+        m_Player_Jump = m_Player.GetAction("Jump");
+        m_Player_Movement = m_Player.GetAction("Movement");
+    }
+
+    ~InputMaster()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes
+    {
+        get => asset.controlSchemes;
+    }
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private InputAction m_Player_Jump;
+    private InputAction m_Player_Movement;
+    public struct PlayerActions
+    {
+        private InputMaster m_Wrapper;
+        public PlayerActions(InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump { get { return m_Wrapper.m_Player_Jump; } }
+        public InputAction @Movement { get { return m_Wrapper.m_Player_Movement; } }
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled { get { return Get().enabled; } }
+        public InputActionMap Clone() { return Get().Clone(); }
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Jump.started += instance.OnJump;
+                Jump.performed += instance.OnJump;
+                Jump.canceled += instance.OnJump;
+                Movement.started += instance.OnMovement;
+                Movement.performed += instance.OnMovement;
+                Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerActions @Player
+    {
+        get
+        {
+            return new PlayerActions(this);
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.GetControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    private int m_KeyboardandMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandMouseSchemeIndex == -1) m_KeyboardandMouseSchemeIndex = asset.GetControlSchemeIndex("Keyboard and Mouse");
+            return asset.controlSchemes[m_KeyboardandMouseSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ SlimeGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+    public LayerMask GroundLayerMask;
+    public float GroundSphere = 0.3f;
+    public bool IsOnGround;
+    
+    public float JumpForce = 4;
+    public float JumpForceHeld = 8;
+    public float JumpTimeTotal = 20f;
+    public float JumpTimeCurrent;
+
+    private InputMaster _inputMaster;
+    private Rigidbody _rigidbody;
+
+    private bool _pressedJump;
+
+    private void Awake() {
+        _inputMaster = new InputMaster();
+
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _inputMaster.Player.Movement.performed += ctx => Move(ctx);
+        _inputMaster.Player.Jump.performed += ctx => Jump(ctx);
+    }
+
+    private void FixedUpdate() {
+        IsOnGround = Physics.CheckSphere(transform.position, GroundSphere, GroundLayerMask);
+        
+        if (_pressedJump) {
+            if (IsOnGround) {
+                _rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+                JumpTimeCurrent = JumpTimeTotal;
+            }
+
+            if (_rigidbody.velocity.y > 1 && JumpTimeCurrent > 0) {
+                _rigidbody.AddForce(Vector3.up * JumpForceHeld, ForceMode.Impulse);
+                JumpTimeCurrent -= 1;
+            }
+        }
+    }
+
+    private void Jump(InputAction.CallbackContext ctx) {
+        Debug.Log("Jumped, " + ctx.ReadValueAsObject());
+
+        // ReSharper disable once SwitchStatementMissingSomeCases
+        switch ((int) Math.Round((float) ctx.ReadValueAsObject())) {
+            case 0:
+                _pressedJump = false;
+                break;
+            case 1:
+                _pressedJump = true;
+                break;
+        }
+    }
+
+    private void Move(InputAction.CallbackContext ctx) {
+        var direction = ctx.ReadValue<Vector2>();
+        Debug.Log("Moved by: " + direction);
+    }
+
+    private void OnEnable() {
+        _inputMaster.Enable();
+    }
+
+    private void OnDisable() {
+        _inputMaster.Disable();
+    }
+}
